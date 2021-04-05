948f4013f1da5173917d589787959550f0adedcb
Farisy1998 <mca560@rajagiri.edu>
2021-04-05 19:04:09 +0530

    changes pushed

--- resources/views/xc90.blade.php
+++ resources/views/xc90.blade.php
@@ -80,7 +80,7 @@
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-1-21x9.jpg?iar=0&w=1366" alt="...">
+      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-6-16x9.jpg?h=700&iar=0" alt="...">
     </div>
     </div>
     </div>
@@ -101,7 +101,7 @@
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-6-16x9.jpg?h=700&iar=0" class="d-block w-100" alt="...">
+      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-1-16x9.jpg?h=700&iar=0" class="d-block w-100" alt="...">
     </div>
     </div>
     </div>
@@ -225,10 +225,10 @@
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12">
         <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-6-16x9.jpg?h=700&iar=0" class="d-block w-100" alt="...">
+      <img src="https://www.volvocars.com/images/v/in/v/-/media/project/contentplatform/data/media/pdp/xc90-fuel/xc90-gallery-3-4x3.jpg?iar=0&w=1366" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h3><font color="black">Volvo Contactless Program</font></h3>
-        <a href="form.blade.php" class="btn btn-outline-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>BUY ONLINE</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <h3><font color="white">Volvo Contactless Program</font></h3>
+        <a href="form.blade.php" class="btn btn-outline-light">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>BUY ONLINE</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
       </div>
     </div>
   </div>
