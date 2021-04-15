c611c940538d276db23629ee11131d350034283f
Farisy1998 <mca560@rajagiri.edu>
2021-04-15 22:53:58 +0530

    Changes pushed

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -38,6 +38,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Volvo-iron.jpg/jcr:content/renditions/1860x1050-Volvo-iron-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+      <h2><b>Volvo</b></h2>
+                <font color="black">The Volvo brand is one of the world’s best known and respected brands within the commercial vehicle industry. It is associated with the core values of quality, safety and environmental care.</font>
+                <br><br><font color="black">'Volvo' is Latin and means 'I roll'</font>
     </div>
     </div>
 </div>
@@ -47,22 +51,12 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Penta.jpg/jcr:content/renditions/1860x1050-Penta-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Volvo Penta</b></h2>
+                <font color="black">Volvo Penta is a world-leading supplier of engines and complete power systems for marine and industrial applications. It is associated with innovative and performance-oriented products in addition to quality, safety and environmental care.</font>
     </div>
     </div>
 </div> 
-            </div>
-        </div>
-        <div class="row">
-            <div class="col col-12 co-sm-6 col-md-6 col-lg-6">
-            <br>
-                <h2><b>Volvo</b></h2>
-                <font color="black">The Volvo brand is one of the world’s best known and respected brands within the commercial vehicle industry. It is associated with the core values of quality, safety and environmental care.</font>
-                <br><br><font color="black">'Volvo' is Latin and means 'I roll'</font>
-            </div>
-            <div class="col ol-12 co-sm-6 col-md-6 col-lg-6">
-            <br>
-            <h2><b>Volvo Penta</b></h2>
-                <font color="black">Volvo Penta is a world-leading supplier of engines and complete power systems for marine and industrial applications. It is associated with innovative and performance-oriented products in addition to quality, safety and environmental care.</font>
             </div>
         </div>
         <br>
@@ -74,6 +68,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Terex.jpg/jcr:content/renditions/1860x1050-Terex-teaser3.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Terex Trucks</b></h2>
+                <font color="black">Terex Trucks products are used around the world in mining, quarrying, and infrastructure applications.</font>
+                <p><font color="black"><br> From arctic to desert conditions, and tropical to dry climates, Terex Trucks’ haulers perform in some of the most extreme environments.</font></p>
     </div>
     </div>
 </div> 
@@ -83,6 +81,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Renault.jpg/jcr:content/renditions/1860x1050-Renault-teaser3.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Renault Trucks</b></h2>
+                <font color="black">Since 1894 Renault Trucks has offered all types of commercial vehicles, ranging from light trucks for urban distribution services to specially designed and heavy trucks for long-haul operations.</font>
+                <p><font color="black"><br>The vehicles and services offering is adapted to different transport applications and helps to reduce customers´ operational costs.</font></p>
     </div>
     </div>
 </div> 
@@ -92,29 +94,13 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Prevost.jpg/jcr:content/renditions/1860x1050-Prevost-teaser3.jpg" class="d-block w-100" alt="...">
-    </div>
-    </div>
-</div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
-            <h2><b>Terex Trucks</b></h2>
-                <font color="black">Terex Trucks products are used around the world in mining, quarrying, and infrastructure applications.</font>
-                <p><font color="black"><br> From arctic to desert conditions, and tropical to dry climates, Terex Trucks’ haulers perform in some of the most extreme environments.</font></p>
-            </div>
-            <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
-            <h2><b>Renault Trucks</b></h2>
-                <font color="black">Since 1894 Renault Trucks has offered all types of commercial vehicles, ranging from light trucks for urban distribution services to specially designed and heavy trucks for long-haul operations.</font>
-                <p><font color="black"><br>The vehicles and services offering is adapted to different transport applications and helps to reduce customers´ operational costs.</font></p>
-            </div>
-            <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
+      <br>
             <h2><b>Prevost</b></h2>
                 <font color="black">With more than 10,000 vehicles on North American roads, Prevost is a leading North American manufacturer of premium seated coaches and conversion coaches for high-end motor homes and specialty conversions.</font>
                 <p><font color="black"><br>Prevost was founded in 1924 and since Eugéne Prevost´s first wooden-frame bus Prevost has been known for its unwavering commitment to quality, innovation and attention to detail.</font></p>
+    </div>
+    </div>
+</div>
             </div>
         </div>
         <br>
@@ -126,6 +112,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-NovaBus.jpg/jcr:content/renditions/1860x1050-NovaBus-teaser3.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Nova Bus</b></h2>
+                <font color="black">Nova Bus is a leading North American provider of sustainable transit solutions, including buses, high-capacity vehicles and integrated intelligent transportation systems.</font>
+                <p><font color="black"><br>The vehicles are the result of an integrated manufacturing process in which expertise meets clients' specific needs and challenges.</font></p>
     </div>
     </div>
 </div>
@@ -135,6 +125,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Mack.jpg/jcr:content/renditions/1860x1050-Mack-teaser3.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Mack</b></h2>
+                <font color="black">Founded in 1900, Mack Trucks – recognised around the world as "The American Truck You Can Count On" – is known for building durable and reliable, application-driven vehicles that get the job done.</font>
+                <p><font color="black"><br>An iconic brand, Mack embodies the American spirit: pioneering, hard-working, authentic and ambitious.</font></p>
     </div>
     </div>
 </div>
@@ -144,28 +138,12 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Arquus.jpg/jcr:content/renditions/1860x1050-Arquus-teaser3.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Arquus</b></h2>
+                <font color="black">Arquus manufactures vehicles adapted for operations in international projects to Governments and organizations worldwide.</font>
     </div>
     </div>
 </div>
-            </div>
-        </div>
-        <div class="row">
-        <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
-            <h2><b>Nova Bus</b></h2>
-                <font color="black">Nova Bus is a leading North American provider of sustainable transit solutions, including buses, high-capacity vehicles and integrated intelligent transportation systems.</font>
-                <p><font color="black"><br>The vehicles are the result of an integrated manufacturing process in which expertise meets clients' specific needs and challenges.</font></p>
-            </div>
-            <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
-            <h2><b>Mack</b></h2>
-                <font color="black">Founded in 1900, Mack Trucks – recognised around the world as "The American Truck You Can Count On" – is known for building durable and reliable, application-driven vehicles that get the job done.</font>
-                <p><font color="black"><br>An iconic brand, Mack embodies the American spirit: pioneering, hard-working, authentic and ambitious.</font></p>
-            </div>
-            <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
-            <br>
-            <h2><b>Arquus</b></h2>
-                <font color="black">Arquus manufactures vehicles adapted for operations in international projects to Governments and organizations worldwide.</font>
             </div>
         </div>
         <br>
@@ -186,6 +164,10 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-SDLG.jpg/jcr:content/renditions/1860x1050-SDLG-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>SDLG</b></h2>
+                <font color="black">SDLG (Shandong Lingong Construction Machinery Co) is sold to customers focused on reliable and competitive equipment primarily in China and other emerging markets.</font>
+                <p><font color="black"><br>The company develops, manufactures and markets a broad range of products including wheel loaders, excavators and backhoe loaders.</font></p>
     </div>
     </div>
 </div>
@@ -195,24 +177,14 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Eicher.jpg/jcr:content/renditions/1860x1050-Eicher-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Eicher</b></h2>
+                <font color="black">Built on 50 years of local presence and international partnerships Eicher is one of the largest players in the Indian mainstream commercial vehicle market, with a strong reputation for fuel efficiency and profitability for customers.</font>
     </div>
     </div>
 </div>
             </div>
             </div>
-        <div class="row">
-            <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
-            <br>
-            <h2><b>SDLG</b></h2>
-                <font color="black">SDLG (Shandong Lingong Construction Machinery Co) is sold to customers focused on reliable and competitive equipment primarily in China and other emerging markets.</font>
-                <p><font color="black"><br>The company develops, manufactures and markets a broad range of products including wheel loaders, excavators and backhoe loaders.</font></p>
-            </div>
-            <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
-            <br>
-            <h2><b>Eicher</b></h2>
-                <font color="black">Built on 50 years of local presence and international partnerships Eicher is one of the largest players in the Indian mainstream commercial vehicle market, with a strong reputation for fuel efficiency and profitability for customers.</font>
-            </div>
-        </div>
         <br>
         <br>
         <br>
@@ -222,6 +194,9 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-Dongfeng.jpg/jcr:content/renditions/1860x1050-Dongfeng-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>Dongfeng Trucks</b></h2>
+                <font color="black">Dongfeng Trucks established in 1969. It originates from the Chinese culture and has further been developed through craftsmanship with trust, professionalism and global techniques.</font>
     </div>
     </div>
 </div>
@@ -231,21 +206,12 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.volvogroup.com/content/dam/volvo/volvo-group/markets/global/en-en/what-we-do/our-brands/1860x1050-cellcentric-temporary-logo.jpg/jcr:content/renditions/1860x1050-cellcentric-temporary-logo-teaser2.jpg" class="d-block w-100" alt="...">
+      <br>
+            <h2><b>cellcentric</b></h2>
+                <font color="black">cellcentric, a 50:50 joint venture between Daimler Truck AG and the Volvo Group, is responsible for all activities along the entire value chain for fuel cell systems: from research and development through production to marketing of fuel cells to areas of application even outside vehicles.</font>
     </div>
     </div>
 </div>
-            </div>
-        </div>
-        <div class="row">
-        <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
-            <br>
-            <h2><b>Dongfeng Trucks</b></h2>
-                <font color="black">Dongfeng Trucks established in 1969. It originates from the Chinese culture and has further been developed through craftsmanship with trust, professionalism and global techniques.</font>
-            </div> 
-            <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
-            <br>
-            <h2><b>cellcentric</b></h2>
-                <font color="black">cellcentric, a 50:50 joint venture between Daimler Truck AG and the Volvo Group, is responsible for all activities along the entire value chain for fuel cell systems: from research and development through production to marketing of fuel cells to areas of application even outside vehicles.</font>
             </div>
         </div>
         <br>
